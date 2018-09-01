ec25535e37592d8b9109c3c76598a978161152ec
BiaDarkia <BiaDarkia@users.noreply.github.com>
2018-09-01 21:21:06 +0900

    R source code to create plots and PNG plots generated by source code

--- plot1.R
+++ plot1.R
@@ -1,3 +1,14 @@
+dlMethod <- "curl"
+if(substr(Sys.getenv("OS"),1,7) == "Windows") dlMethod <- "wininet"
+if(!file.exists("household_power_consumption.txt")) {
+    download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
+        destfile="household_power_consumption.zip",
+        method=dlMethod, # use OS-appropriate method
+        mode="wb")        # "w" means "write," and is used for text files
+    unzip(zipfile = "household_power_consumption.zip")
+    file.remove("household_power_consumption.zip")
+}
+
 data <- read.table("./household_power_consumption.txt", sep = ";", skip = 66637, nrows = 2881, 
     col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", 
     "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
